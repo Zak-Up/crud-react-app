2f66b784e14f4f844e0888e5f5338b891429f32d
ZakyUp <andresjr195@hotmail.com>
2022-05-04 20:36:23 -0500

    ejercicios

---
+++ src/components/CrudApp.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+
+const initialDB = [
+    {
+        id: 1,
+        name: 'Seiya',
+        constellation: 'Pegaso',
+    },
+    {
+        id: 2,
+        name: 'Shiryu',
+        constellation: 'Dragón',
+    },
+    {
+        id: 3,
+        name: 'Hyoga',
+        constellation: 'Cisne',
+    },
+    {
+        id: 4,
+        name: 'Shun',
+        constellation: 'Andrómeda',
+    },
+    {
+        id: 5,
+        name: 'Ikki',
+        constellation: 'Fénix',
+    }
+]
+
+const CrudApp = () => {
+    const [db, setDb] = useState(initialDB);
+
+    const [dataToEdit, setDataToEdit] = useState(null);
+
+    const createData = (data) => {
+        console.log(data)
+    };
+
+    const updateData = (data) => {};
+
+    const deleteData = (id) => {};
+
+    return (
+        <div>
+            <h2>CRUD APP</h2>
+            <CrudForm 
+            createData={createData} 
+            updateData={updateData} 
+            dataToEdit={dataToEdit} 
+            setDataToEdit={setDataToEdit}
+            />
+            <CrudTable 
+            data={db}
+            setDataToEdit={setDataToEdit}
+            deleteData = {deleteData}
+            />
+        </div>
+    )
+}
+
+export default CrudApp
